6443c6dc80f5f0212e4c516196580696bb4a29a2
Darren Kiely <darrenkiely@hotmail.com>
2018-02-08 16:48:38 +0000

    added additional exception handling and test for status codes report

--- http_test.py
+++ http_test.py
@@ -1,30 +1,45 @@
 import unittest
-from request_maker import readInputUrls, write_json_to_file
+import json
+from request_maker import readInputUrls, write_json_to_file, retrieve_response_details, status_codes_report
 
 class TestUrls(unittest.TestCase):
 
 
     def test_url_file_read(self):
-        self.assertEqual(readInputUrls("urls.txt"),"http://www.google.com")
+        self.assertEqual(readInputUrls("urls_test"),["http://www.google.com"])
 
 
     def test_response_details(self):
-        url = "http://www.google.com"
+        url = "https://www.yahoo.com"
         expected_status_code = 200
-        expected_content_length = 5398
+        expected_content_length = "12"
         expected_date = "figure this out"
         expected_response_details = {"URL": url, "Status_code": expected_status_code,
                                      "Content_length": expected_content_length, "Date": expected_date}
+        actual_response_details = json.loads(retrieve_response_details(url)[0])
+        self.assertEqual(expected_response_details["URL"], actual_response_details["URL"])
+        self.assertEqual(expected_response_details["Status_code"], actual_response_details["Status_code"])
+        self.assertEqual(expected_response_details["Content_length"], actual_response_details["Content_length"])
+
 
 
     def test_file_write(self):
         expected_json = ("{\"URL\": \"https://google.com\", \"Status_code\": 302, \"Content_length\": \"269\","
                      "\"Date\": \"Wed, 07 Feb 2018 19:13:16 GMT\"}")
-        write_json_to_file("https://google.com")
+
+        write_json_to_file(expected_json)
         with open("REPORT----google.txt") as file:
             content = file.read()
 
         self.assertEqual(content,expected_json)
+
+    def test_all_status_records(self):
+        status_codes = [200,302,200]
+        expected_status_codes_json = [{"Status_code": 200, "Number_of_responses": 2},
+                                      {"Status_code": 302, "Number_of_responses": 1}]
+        actual_status_codes_json = status_codes_report(status_codes)
+        json.load(open("status_reports.txt"))
+        self.assertEqual(expected_status_codes_json, actual_status_codes_json)
 
 
 def main():
--- request_maker.py
+++ request_maker.py
@@ -9,6 +9,10 @@ import json
 # # print r.headers['Content-length']
 # print(t.headers)
 
+
+
+status_codes = []
+
 def readInputUrls(filename):
     with open(filename)as file:
         try:
@@ -22,31 +26,48 @@ def readInputUrls(filename):
 
 
 def retrieve_response_details(url):
-    response = requests.head(url)
-    status_code = response.status_code
-    date = response.headers['Date']
-    content_length = response.headers["Content-Length"]
+    try:
+        response = requests.head(url, timeout=10)
+        status_code = response.status_code
+        date = response.headers['Date']
+        content_length = response.headers["Content-Length"]
+    except Exception as e:
+        details = {"URL": url, "Message": "invalid url"}
+        jsonified_details = json.dumps(details, indent=4)
+        response = [jsonified_details]
+        return response
 
     details = {"URL": url, "Status_code": status_code, "Content_length": content_length, "Date": date}
 
-    jsonified_details = json.dumps(details)
+    jsonified_details = json.dumps(details, indent=4)
 
-    return jsonified_details
+    response = [jsonified_details, status_code]
+
+    return response
 
 def write_json_to_file(jsom):
     json_data = json.loads(jsom)
     url = json_data['URL']
     site = url.split("//")[1]
-    site_name = site.split(".")[0]
+    if "www." in site:
+        site_name = site.split(".")[1]
+    else:
+        site_name = site.split(".")[0]
     # file = open("llamas.txt", "w+")
     file = open("REPORT----"+site_name+".txt","w+")
     # with open("site-"+site+"---Response-details.txt","w") as file:
     file.write(jsom)
+
+def status_codes_report(status_codes):
 
 
 if __name__ == '__main__':
     urls = readInputUrls("urls")
     json_details = []
     for url in urls:
-        json_details = retrieve_response_details(url)
-        write_json_to_file(json_details)
+        response = retrieve_response_details(url)
+        write_json_to_file(response[0])
+        if len(response)>1:
+            status_codes.append(response[1])
+
+    print "done"
